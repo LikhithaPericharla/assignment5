b556a143bd67aecb58289709f42b593201358562
LikhithaPericharla <42575000+LikhithaPericharla@users.noreply.github.com>
2020-07-17 22:16:43 +0530

    Add files via upload

---
+++ Interest.java
@@ -0,0 +1,10 @@
+package epam;
+
+public class Interest {
+	public double Simple_Interest(double p,double r,double t){
+ 	   return (p*r*t)/100;
+    }
+    public double Compound_Interest(double p,double r,double t){
+ 	   return (p*Math.pow(1+(r/100),t));
+    }
+}
